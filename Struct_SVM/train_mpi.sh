ba0753e4f1d5e5c128e5f30e303cb0d60fd982ff
Yu Xiang <yuxiang@napoli101.Stanford.EDU>
2014-05-16 21:31:36 -0700

    tmp

--- Struct_SVM/train_mpi.sh
+++ Struct_SVM/train_mpi.sh
@@ -4,6 +4,5 @@ CLS=car
 DATADIR=../Main/data/
 
 # Perform tasks
-echo "mpirun -np 96 --hostfile machinefile ./svm_struct_learn -c 100000000 -l 0 -w 1 --l 100 --w 10 --f 0 --p 5 --h 0 $DATADIR$CLS"_wrap.dat" $DATADIR$CLS"_unwrap.dat" $DATADIR$CLS"_neg.dat" $DATADIR$CLS".cad" | tee $DATADIR$CLS"_train.log""
-
-mpirun -np 96 --hostfile machinefile ./svm_struct_learn -c 100000000 -l 0 -w 1 --l 100 --w 10 --f 0 --p 5 --h 0 $DATADIR$CLS"_wrap.dat" $DATADIR$CLS"_unwrap.dat" $DATADIR$CLS"_neg.dat" $DATADIR$CLS".cad" | tee $DATADIR$CLS"_train.log"
+cat train_mpi.sh
+mpirun -np 96 --hostfile machinefile ./svm_struct_learn -c 100000000 -l 0 -w 1 --l 100 --w 10 --f 1 --p 5 --h 5 $DATADIR$CLS"_wrap.dat" $DATADIR$CLS"_unwrap.dat" $DATADIR$CLS"_neg.dat" $DATADIR$CLS".cad" | tee -a $DATADIR$CLS"_train.log"
